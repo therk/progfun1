9f262e457c1a57e2d0809c1d333212ec9661be70
Ruslan Kharitonov <therk@akumen.net>
2016-05-27 17:56:55 -0400

    Implement the balance function and added .gitignore to avoid intellij .idea and other . files.

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,41 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r)
+      return 1
+    else if (c < 0 || c > r)
+      return 0
+    else
+      return pascal(c-1, r-1) + pascal(c, r-1)
+  }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+
+    def balanceCounter(count: Int, remainingChars: List[Char]):Boolean = {
+      if(remainingChars.length == 0) {
+        return count == 0
+      }
+      val head = remainingChars.head
+      val c = if (head == '(') count + 1 else if (head == ')') count - 1 else count
+      // if c there are more closing ')' that means the count will be less then 0, so unbalanced
+      if (c < 0)
+        return false
+      else
+        balanceCounter(c, remainingChars.tail)
+    }
+
+    balanceCounter(0, chars)
+  }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+
+    1
+    }
   }
